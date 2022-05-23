ed294c737a3d8ed2e59731fe64ecc9c09e68cf08
doha  khaled <66404704+dohakhaled33@users.noreply.github.com>
2022-05-24 01:48:03 +0200

    Update DataAnalysis.java

--- Twitter_Project/src/DataAnalysis.java
+++ Twitter_Project/src/DataAnalysis.java
@@ -202,4 +202,151 @@ public class DataAnalysis {
         }
     }
 
+                               
+    /*
+    ⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙
+    ⁛                                 👉 The bonus section ❤                                 ⁛
+    ⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙⁙
+    */
+    // Complexity -> O(E).
+    public static Set<Followers_Set> followersReader(Set<Followees_Set> followeesSet) throws FileNotFoundException {
+        Set<Followers_Set> followersSet = new TreeSet<>();
+
+        // Complexity -> O(E).
+        for (Followees_Set acc : followeesSet){                     // Copy data from the Followees Set into the Followers Set
+            followersSet.add(new Followers_Set(acc.follower, acc.followee));
+        }
+
+        File csvFile = new File("Sorted_Followers.csv");   // Storing the sorted data into CSV file.
+        PrintWriter out = new PrintWriter(csvFile);
+        // Complexity -> O(E).
+        for (Followers_Set acc : followersSet) {
+            out.println(acc.follower + ", " + acc.followee);
+        }
+        out.close();
+
+        return followersSet;
+    }
+
+    // Complexity -> O(E).
+    public static int nFollowers(Set<Followers_Set> followersSet) {
+        int nFollowers = 0;                                         // Counter.
+        int preID = -1;                                             // Previous ID.
+        // Complexity -> O(E).
+        for (Followers_Set acc : followersSet) {
+            if (acc.follower != preID) {                            // If the read follower is not the previous one.
+                nFollowers++;                                       // Increment the counter.
+                preID = acc.follower;                               // Make previous pointing to the current follower.
+            }
+        }
+        return nFollowers;
+    }
+
+    // Complexity -> O(E).
+    public static Follower_NewFriend[] insertFollowers(Set<Followers_Set> followersSet, int nFollowers) {
+        // Declare an array of Followees.
+        Follower_NewFriend[] followers = new Follower_NewFriend[nFollowers];
+        int i = 0;
+
+        // Complexity -> O(E).
+        for (Followers_Set account : followersSet) {
+            // Create a new followee.
+            Followee_NewFriend newFollowee = new Followee_NewFriend(account.followee);
+
+            if (followers[0] == null) {                             // If the array is empty.
+
+                followers[0] = new Follower_NewFriend(account.follower, newFollowee);
+
+            } else if (account.follower == followers[i].id) {       // If the read followee belongs to the same followee.
+
+                newFollowee.nextFollowee = followers[i].followee;   // Change references.
+                followers[i].followee = newFollowee;
+                followers[i].nFollowees++;                          // Increment number of followees for the current follower.
+
+            } else if (account.follower != followers[i].id) {       // If the read followee doesn't belong to the same follower.
+                i++;
+                followers[i] = new Follower_NewFriend(account.follower, newFollowee);
+            }
+        }
+        return followers;
+    }
+
+    // Complexity -> O(log(V)).
+    public static int getFollowerIndex(Follower_NewFriend[] followers, int id, int low, int high) {
+        // Using D&C Algorithm (Binary Search).
+        int mid = (low + high) / 2;
+
+        if (low > high) {
+            System.out.println("Sorry, not found!");
+            return -1;
+        }
+        if (followers[mid].id == id) {                              // The follower is found.
+            return mid;
+        } else if (followers[mid].id < id) {
+            return getFollowerIndex(followers, id, mid + 1, high);
+        } else {
+            return getFollowerIndex(followers, id, low, mid - 1);
+        }
+    }
+
+    // Complexity -> O(v).
+    public static boolean isRecommended(Set<ComFriends> allFollowees, int sugID, int thresholdNum) {
+
+        int preFollowee = 0;
+
+        // Complexity -> O(v).
+        for (ComFriends acc : allFollowees) {
+            if (acc.followee == sugID) {                            // If the entered ID already follows the possible suggested ID.
+                return false;
+            } else {
+                if (acc.followee == preFollowee) {                  // If we found a repeated followee (we have a mutual followee).
+                    thresholdNum--;                                 // Decrement the threshold number.
+                } else {
+                    preFollowee = acc.followee;
+                }
+            }
+        }
+        return thresholdNum <= 0;
+    }
+
+    // Complexity -> O(v^2).
+    public static List<Integer> suggestFriends(Follower_NewFriend[] followers, int nFollowers, int id, int thresholdNum){
+        Set<ComFriends> allFollowees = new TreeSet<>();             // A buffer contains followees of both followers.
+        List<Integer> sugFriends = new ArrayList<>();               // Array List contains the suggested friends.
+
+        // Get the index of the entered ID.
+        int index = getFollowerIndex(followers, id, 0, nFollowers);
+
+        // Complexity -> O(v^2).
+        for (int i = 0 ; i < nFollowers; i++) {                     // Iterate on all accounts.
+
+            if (followers[i].id != id) {
+
+                // Adding the (id)'s followees into allFollowees
+                Followee_NewFriend current = followers[index].followee;
+                // Complexity -> O(v).
+                for (int j = 0; j < followers[index].nFollowees; j++){
+                    allFollowees.add(new ComFriends(id, current.id));
+                    current = current.nextFollowee;
+                }
+
+                // Adding the (i)'s followees into allFollowees
+                current = followers[i].followee;
+                // Complexity -> O(v).
+                for (int k = 0; k < followers[i].nFollowees; k++) {
+                    allFollowees.add(new ComFriends(followers[i].id, current.id));
+                    current = current.nextFollowee;
+                }
+
+                // If the mutual followees >= Threshold number, add the current id to the Suggested Friends List.
+                if (isRecommended(allFollowees, followers[i].id, thresholdNum)){
+                    sugFriends.add(followers[i].id);
+                }
+
+                allFollowees.clear();                               // Clear the buffer for a new possible suggested friend.
+            }
+        }
+        return sugFriends;
+    }                           
+                               
 }
